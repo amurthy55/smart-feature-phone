7334326100d6fb92d7bb30759f1c72e83866269d
amurthy55 <25ds2000003@ds.study.iitm.ac.in>
2026-07-24 07:24:59 +0530

    Update send_motor_on.py

--- send_motor_on.py
+++ send_motor_on.py
@@ -3,9 +3,9 @@ import os
 import config
 
 # Replace with your actual values from https://my.telegram.org
-API_ID = 33876152  # Your API ID
-API_HASH = "87a08340b095a7bfca0af3e16cd781c7"  # Your API Hash
-PHONE_NUMBER = "+916379228576"  # Your phone number with country code
+API_ID = xxx  # Your API ID
+API_HASH = "xx"  # Your API Hash
+PHONE_NUMBER = "+xxx"  # Your phone number with country code
 
 CHAT_ID = int(config.MOTOR_TELE_GROUP_ID) if hasattr(config, "MOTOR_TELE_GROUP_ID") else -1003232090112
 MISSED_MSG = "MISSED CALL Command Incoming"
